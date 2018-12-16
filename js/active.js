044d2d8973a6666a2a339d39d0e469e5d0f64011
kemtol <mkemalw@gmail.com>
2018-12-16 15:00:19 +0700

    Update active.js

--- js/active.js
+++ js/active.js
@@ -21,7 +21,7 @@
             smartSpeed: 1000,
             responsive: {
                 0: {
-                    items: 1
+                    items: 2
                 },
                 576: {
                     items: 2
